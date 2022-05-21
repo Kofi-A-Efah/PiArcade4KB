dee51ae1981115c991be0188edc21a03b954be68
Kofi Efah <54334161+Kofi-A-Efah@users.noreply.github.com>
2022-05-21 13:08:01 -0400

    Update client_GPIO.py

--- client_GPIO.py
+++ client_GPIO.py
@@ -19,31 +19,37 @@ server_address = ('10.48.47.103', 10000)
 print("Connecting to " + server_address[0] + " port " + str(server_address[1]) )
 sock.connect(server_address)
 
+#Choosing game 1 or 2 for button configuration purposes
 print("1 for game 1, 2 for game 2: ")
 game = input()
 
 assert (game == '1' or game == '2') 
 sock.sendall(game.encode())
 
-
+#removing file
 if os.path.exists("Log.txt"):
     os.remove("Log.txt")
-    
+
+#Creating new log file
 file1 = open("/home/pi/PiArcade4KB/Log.txt","w")
+
 while(loop):
     try:
       time.sleep(0.025)
 
-      if ( not GPIO.input(23) and GPIO.input(27) ):
+      #Quit
+      if ( not GPIO.input(23)):
           loop = False
           message.append('quit')
 
+      #Sending multiple inputs at once (simultaneous in real life), the message buffer must contain multiple inputs
       if len(message) >= 2:
           button = str(message)
           message.clear()
           file1.write(button + " " + "\n")
           sock.sendall(button.encode())
           print("Sent " + str(button) + " " +" to the computer")
+      #Sending single inputs
       elif len(message) > 0:
           button = str(message)
           message.clear()
